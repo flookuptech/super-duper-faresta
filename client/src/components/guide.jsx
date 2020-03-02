e78abf33471265601bfa604d4890c4ea7e10e80f
Aditya Sanil <aditya.sanil@somaiya.edu>
2020-03-03 00:53:22 +0530

    Update guide.jsx

--- client/src/components/guide.jsx
+++ client/src/components/guide.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const Guide = () => {
-  return <div>Welcome to guide page</div>;
+  return (
+    <Fragment>
+      <div>Welcome to guide page</div>
+      <a href="https://fixed-asset-flookup.s3.ap-south-1.amazonaws.com/FAR-Template.csv">
+        CSV Template
+      </a>
+    </Fragment>
+  );
 };
 
 export default Guide;
